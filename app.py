24914790ca647df4ac9973ae3b6a520760543cb2
zhangjianyu1006156 <113819145+zhangjianyu1006156@users.noreply.github.com>
2025-11-14 03:24:20 +0000

    1

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    q = float(request.form.get("q"))
+    return(render_template("prediction.html", r=(-50.6*q)+90.2))
 
 if __name__ == "__main__":
     app.run()
